2d77205402d2d6295c1e6cec34c05bcfeafba29b
unknown <ruoyu.zhang@student.adelaide.edu.au>
2025-03-31 06:43:40 +1030

    Created socket and connected to origin web server on cache miss

--- Proxy.py
+++ Proxy.py
@@ -134,6 +134,7 @@ while True:
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
+    originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
@@ -142,6 +143,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+      originServerSocket.connect((address, 80))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
